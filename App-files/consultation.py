806175d834a85d5d813305dc812cd15fceef15c2
Alexander Waite <a.waite1703@gmail.com>
2025-03-21 23:09:35 -0500

    Update consultation.py

--- App-files/consultation.py
+++ App-files/consultation.py
@@ -5,10 +5,11 @@ from person import db
 class Consultation: 
 
     id = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key=True)
-    consultationId = db.Column(db.Integer, unique=True, nullable=False, autoincrement=True)  # Auto-generated ID
+    consultationId = db.Column(db.Integer, unique=True, nullable=False, autoincrement=True) 
     date = db.Column(db.String(20), nullable=False)
     notes = db.Column(db.String(20), nullable=True)
     preferences = db.Column(db.String(100), nullable=True)
+    confirmed = db.Column(db.Boolean, default=False)
 
 
     def __init__(self, consultationId, date, notes, preferences):
@@ -19,10 +20,16 @@ class Consultation:
         self.preferences = preferences 
 
     def addConsultation(self):
-        db.session.add(self) #used chatgpt 
+        db.session.add(self) 
         db.session.commit() 
 
     def updatePreferences(self,preferences):
         self.preferences = preferences 
 
+    def viewPreferences(self):
+        return self.preferences
+
+    def confirmConsultation(self):
+        self.confirmed = True
+        db.session.commit()
     
