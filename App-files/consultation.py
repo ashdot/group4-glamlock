7ba76c5cb7afa39ebc6f7b78073d8e27f5ae2f1b
Alexander Waite <a.waite1703@gmail.com>
2025-04-09 11:25:12 -0500

    consultation.py
    annotated the consultation class

--- App-files/consultation.py
+++ App-files/consultation.py
@@ -9,9 +9,9 @@ class Consultation:
     date = db.Column(db.String(20), nullable=False)
     notes = db.Column(db.String(20), nullable=True)
     preferences = db.Column(db.String(100), nullable=True)
-    confirmed = db.Column(db.Boolean, default=False)
+    confirmed = db.Column(db.Boolean, default=False) #boolean that shows whether a consulation is confirmed or not
 
-
+    #Constuctor that initialises a consultation instance 
     def __init__(self, consultationId, date, notes, preferences):
 
         self.consultationId = consultationId
@@ -19,17 +19,18 @@ class Consultation:
         self.notes = notes
         self.preferences = preferences 
 
+    # Adds the current Consultation object to the database and commits the transaction
     def addConsultation(self):
         db.session.add(self) 
         db.session.commit() 
-
+    # Updates the preferences of the consultation and commits the change.
     def updatePreferences(self,preferences):
         self.preferences = preferences
         db.session.commit()
-
+    # Returns the current preferences of the consultation.
     def viewPreferences(self):
         return self.preferences
-
+    # Marks the consultation as confirmed and commits the change.
     def confirmConsultation(self):
         self.confirmed = True
         db.session.commit()
